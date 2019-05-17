c268a6aeeaeaaef72dbc1d95012f2aa2b8aa24dc
Jacek <ja77co@wp.pl>
2019-05-17 10:16:11 +0200

    Zadanie5.1 Pokój z klimatyzacją Utworzenie kodu + dodany konstruktor na 2

--- src/Room.java
+++ src/Room.java
@@ -1,12 +1,41 @@
 public class Room {
-    double roomArea;
-    double currentTemperature;
-    boolean isAirConditioner;
+    private double roomArea;
+    private double currentTemperature;
+    private boolean isAirConditioner;
 
-    public Room(double roomArea, double currentTemperature, boolean isAirConditioner) {
+    public double getRoomArea() {
+        return roomArea;
+    }
+
+    public double getCurrentTemperature() {
+        return currentTemperature;
+    }
+
+    public boolean isAirConditioner() {
+        return isAirConditioner;
+    }
+
+    public void setRoomArea(double roomArea) {
+        this.roomArea = roomArea;
+    }
+
+    public void setCurrentTemperature(double currentTemperature) {
+        this.currentTemperature = currentTemperature;
+    }
+
+    public void setAirConditioner(boolean airConditioner) {
+        isAirConditioner = airConditioner;
+    }
+
+    public Room(double roomArea, double currentTemperature) {
         this.roomArea = roomArea;
         this.currentTemperature = currentTemperature;
+    }
+
+    public Room(double roomArea, double currentTemperature, boolean isAirConditioner) {
+        this (roomArea, currentTemperature);
         this.isAirConditioner = isAirConditioner;
+
     }
     boolean lowerTemperature (){
         if (isAirConditioner == true && currentTemperature > 18) {
--- src/Test.java
+++ src/Test.java
@@ -1,21 +1,25 @@
 public class Test {
     public static void main(String[] args) {
 
-        Room room1 = new Room (10, 20, true);
+        Room room1 = new Room (10, 20,true);
         Room room2 = new Room (20, 15,true);
         Room room3 = new Room (30, 20,false);
+        Room room4 = new Room (30, 20);
 
-        System.out.println(room1.roomArea +" " +room1.currentTemperature +" " +room1.isAirConditioner);
-        System.out.println(room2.roomArea +" " +room2.currentTemperature +" " +room2.isAirConditioner);
-        System.out.println(room3.roomArea +" " +room3.currentTemperature +" " +room3.isAirConditioner);
+
+        System.out.println(room1.getRoomArea() +" " +room1.getCurrentTemperature() +" " +room1.isAirConditioner());
+        System.out.println(room2.getRoomArea() +" " +room2.getCurrentTemperature() +" " +room2.isAirConditioner());
+        System.out.println(room3.getRoomArea() +" " +room3.getCurrentTemperature() +" " +room3.isAirConditioner());
+        System.out.println(room4.getRoomArea() +" " +room4.getCurrentTemperature() +" " +room4.isAirConditioner());
 
        /* room1.lowerTemperature();
         room2.lowerTemperature();
         room3.lowerTemperature();
        */
-        System.out.println(room1.lowerTemperature() +" "+room1.roomArea +" " +room1.currentTemperature +" " +room1.isAirConditioner);
-        System.out.println(room2.lowerTemperature() +" "+room2.roomArea +" " +room2.currentTemperature +" " +room2.isAirConditioner);
-        System.out.println(room3.lowerTemperature() +" "+room3.roomArea +" " +room3.currentTemperature +" " +room3.isAirConditioner);
+        System.out.println(room1.lowerTemperature() +" "+room1.getRoomArea() +" " +room1.getCurrentTemperature() +" " +room1.isAirConditioner());
+        System.out.println(room2.lowerTemperature() +" "+room2.getRoomArea() +" " +room2.getCurrentTemperature() +" " +room2.isAirConditioner());
+        System.out.println(room3.lowerTemperature() +" "+room3.getRoomArea() +" " +room3.getCurrentTemperature() +" " +room3.isAirConditioner());
+        System.out.println(room4.lowerTemperature() +" "+room4.getRoomArea() +" " +room4.getCurrentTemperature() +" " +room4.isAirConditioner());
 
 
 
